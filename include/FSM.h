5421ead10bfb690b932bd7150a6427743977b122
Konstantin Gorbenko <kyb.6.granei@yandex.ru>
2024-02-08 13:02:38 +0300

    Делаю FSM

---
+++ include/FSM.h
@@ -0,0 +1,11 @@
+#pragma once
+
+void fsm_init();
+void fsm_update();
+void fsm_init_state ();
+void fsm_drawECUErrors_state ();
+void fsm_drawECUParameters1_state ();
+void fsm_drawABSParameters_state ();
+void fsm_drawECUParameters2_state ();
+void fsm_drawABSErrors_state ();
+void fsm_drawFanStatus_state ();
---
+++ src/FSM.cpp
@@ -0,0 +1,165 @@
+#include "include/FSM.h"
+
+
+// https://menginventor.github.io/FSM_coder/#
+
+// void setup(){
+//     fsm_init();//initialize FSM, Needed
+// }
+// void loop(){
+//     fsm_update();//update FSM, Needed
+// }
+
+
+// This section gennerated by FSM Coder
+void ( *fsm_state )( void ); // use function pointer as state variable;
+bool fsm_enter_state_flag = true;
+void fsm_init()
+{
+	fsm_state = &fsm_init_state;
+}
+
+void fsm_update()
+{
+	( *fsm_state )(); // call FSM state
+}
+
+void fsm_init_state()
+{
+	// Declare local/static variable here.
+
+	if ( fsm_enter_state_flag )
+	{
+		// Run once when enter this state.
+	}
+	// Run repeatly for update.
+
+
+	if ( true )
+	{
+		fsm_state = &fsm_drawECUErrors_state;
+		fsm_enter_state_flag = true;
+		return;
+	}
+	fsm_enter_state_flag = false; // Reset flag
+}
+
+void fsm_drawECUErrors_state()
+{
+	// Declare local/static variable here.
+
+	if ( fsm_enter_state_flag )
+	{
+		// Run once when enter this state.
+	}
+	// Run repeatly for update.
+
+
+	if ( true )
+	{
+		fsm_state = &fsm_drawECUParameters1_state;
+		fsm_enter_state_flag = true;
+		return;
+	}
+	fsm_enter_state_flag = false; // Reset flag
+}
+
+void fsm_drawECUParameters1_state()
+{
+	// Declare local/static variable here.
+
+	if ( fsm_enter_state_flag )
+	{
+		// Run once when enter this state.
+	}
+	// Run repeatly for update.
+
+
+	if ( true )
+	{
+		fsm_state = &fsm_drawECUParameters2_state;
+		fsm_enter_state_flag = true;
+		return;
+	}
+	fsm_enter_state_flag = false; // Reset flag
+}
+
+void fsm_drawABSParameters_state()
+{
+	// Declare local/static variable here.
+
+	if ( fsm_enter_state_flag )
+	{
+		// Run once when enter this state.
+	}
+	// Run repeatly for update.
+
+
+	if ( true )
+	{
+		fsm_state = &fsm_drawECUErrors_state;
+		fsm_enter_state_flag = true;
+		return;
+	}
+	fsm_enter_state_flag = false; // Reset flag
+}
+
+void fsm_drawECUParameters2_state()
+{
+	// Declare local/static variable here.
+
+	if ( fsm_enter_state_flag )
+	{
+		// Run once when enter this state.
+	}
+	// Run repeatly for update.
+
+
+	if ( true )
+	{
+		fsm_state = &fsm_drawFanStatus_state;
+		fsm_enter_state_flag = true;
+		return;
+	}
+	fsm_enter_state_flag = false; // Reset flag
+}
+
+void fsm_drawABSErrors_state()
+{
+	// Declare local/static variable here.
+
+	if ( fsm_enter_state_flag )
+	{
+		// Run once when enter this state.
+	}
+	// Run repeatly for update.
+
+
+	if ( true )
+	{
+		fsm_state = &fsm_drawABSParameters_state;
+		fsm_enter_state_flag = true;
+		return;
+	}
+	fsm_enter_state_flag = false; // Reset flag
+}
+
+void fsm_drawFanStatus_state()
+{
+	// Declare local/static variable here.
+
+	if ( fsm_enter_state_flag )
+	{
+		// Run once when enter this state.
+	}
+	// Run repeatly for update.
+
+
+	if ( true )
+	{
+		fsm_state = &fsm_drawABSErrors_state;
+		fsm_enter_state_flag = true;
+		return;
+	}
+	fsm_enter_state_flag = false; // Reset flag
+}
