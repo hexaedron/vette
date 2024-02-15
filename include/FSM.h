1e4096d82dd2048afb74bc5e574755128c6f2b2f
Konstantin Gorbenko <kyb.6.granei@yandex.ru>
2024-02-15 18:52:03 +0300

    Добавил ещё один статус - fsm_connectECU_state()

--- include/FSM.h
+++ include/FSM.h
@@ -9,6 +9,9 @@ void fsm_drawABSParameters_state ();
 void fsm_drawECUParameters2_state ();
 void fsm_drawABSErrors_state ();
 void fsm_drawFanStatus_state ();
+void fsm_connectECU_state();
 
 void waitForECMSync(void);
 void getADLDData(void);
+void makeScreen(uint8_t x, uint8_t y, const unsigned char *input, uint8_t width, uint8_t height);
+void makeStartScreen(void);
--- src/FSM.cpp
+++ src/FSM.cpp
@@ -54,21 +54,39 @@ void fsm_init_state()
         ALDL_UART.beginHD(8192);
 
         OLEDScreen.init();
-        OLEDScreen.drawImage(10, 1, vette_logo, 88, 64, 0);
-        OLEDScreen.drawchar(6, 15, 'P', 1);
-        OLEDScreen.drawchar(6, 25, 'r', 1);
-        OLEDScreen.drawchar(6, 35, 'e', 1);
-        OLEDScreen.drawchar(6, 45, 's', 1);
-        OLEDScreen.drawchar(6, 55, 's', 1);
-
-        OLEDScreen.drawchar(116, lineNumbers[0], 'B', 1);
-        OLEDScreen.drawchar(116, lineNumbers[1], 'u', 1);
-        OLEDScreen.drawchar(116, lineNumbers[2], 't', 1);
-        OLEDScreen.drawchar(116, lineNumbers[3], 't', 1);
-        OLEDScreen.drawchar(116, lineNumbers[4], 'o', 1);
-        OLEDScreen.drawchar(116, lineNumbers[5], 'n', 1);
-        OLEDScreen.drawFrame(1);
+        makeStartScreen();
         OLEDScreen.refresh();
+	}
+	// Run repeatly for update.
+
+
+	if ( btnPressed(PC6) )
+	{
+		fsm_state = &fsm_connectECU_state;
+		fsm_enter_state_flag = true;
+		return;
+	}
+
+	fsm_enter_state_flag = false; // Reset flag
+}
+
+void fsm_connectECU_state()
+{
+	// Declare local/static variable here.
+
+	if ( fsm_enter_state_flag )
+	{
+		// Run once when enter this state.
+		makeScreen(87, 0, 0, 32, 8);
+		OLEDScreen.drawstr(16, lineNumbers[2], (char*)"Initializing PCM", 1);
+		OLEDScreen.drawstr(26, lineNumbers[3], (char*)"Connection...", 1);
+		OLEDScreen.refresh();
+
+		// We wait for ECM response. If there is no response, we have nothing to do here.
+		waitForECMSync();
+
+		// Populate ALDLData
+		getADLDData();
 	}
 	// Run repeatly for update.
 
@@ -92,26 +110,13 @@ void fsm_drawECUErrors_state()
 	if ( fsm_enter_state_flag )
 	{
 		// Run once when enter this state.
-		OLEDScreen.setbuf(0);
-		OLEDScreen.drawFrame(1);
-		OLEDScreen.drawstr(16, lineNumbers[2], (char*)"Initializing PCM", 1);
-		OLEDScreen.drawstr(26, lineNumbers[3], (char*)"Connection...", 1);
-		OLEDScreen.refresh();
-
-		// We wait for ECM response. If there is no response, we have nothing to do here.
-		waitForECMSync();
-
-		// Populate ALDLData
-		getADLDData();
 		
 		// Here we should parse errors
 		char buf[4] = {0};
 		itoa(ALDLData.TIME, buf, 10);
 		OLEDScreen.drawstr(15, lineNumbers[3], buf, 1);
 
-		OLEDScreen.setbuf(0);
-		OLEDScreen.drawFrame(1);
-		OLEDScreen.drawImage(87, 0, errors_bitmap, 32, 8, 0);
+		makeScreen(87, 0, errors_bitmap, 32, 8);
 		OLEDScreen.refresh();
 	}
 	// Run repeatly for update.
@@ -135,9 +140,7 @@ void fsm_drawECUParameters1_state()
 	if ( fsm_enter_state_flag )
 	{
 		// Run once when enter this state.
-		OLEDScreen.setbuf(0);
-		OLEDScreen.drawFrame(1);
-		OLEDScreen.drawImage(87, 0, params_bitmap, 32, 8, 0);
+		makeScreen(87, 0, params_bitmap, 32, 8);
 		OLEDScreen.refresh();
 	}
 	// Run repeatly for update.
@@ -161,9 +164,7 @@ void fsm_drawABSParameters_state()
 	if ( fsm_enter_state_flag )
 	{
 		// Run once when enter this state.
-		OLEDScreen.setbuf(0);
-		OLEDScreen.drawFrame(1);
-		OLEDScreen.drawImage(94, 0, abs_bitmap, 16, 8, 0);
+		makeScreen(94, 0, abs_bitmap, 16, 8);
 		OLEDScreen.drawImage(24, 16, vette_top_bitmap, 64, 32, 0);
 		OLEDScreen.refresh();
 	}
@@ -208,9 +209,7 @@ void fsm_drawABSErrors_state()
 	if ( fsm_enter_state_flag )
 	{
 		// Run once when enter this state.
-		OLEDScreen.setbuf(0);
-		OLEDScreen.drawFrame(1);
-		OLEDScreen.drawImage(84, 0, abs_err_bitmap, 32, 8, 0);
+		makeScreen(84, 0, abs_err_bitmap, 32, 8);
 		OLEDScreen.refresh();
 	}
 	// Run repeatly for update.
@@ -234,9 +233,7 @@ void fsm_drawFanStatus_state()
 	if ( fsm_enter_state_flag )
 	{
 		// Run once when enter this state.
-		OLEDScreen.setbuf(0);
-		OLEDScreen.drawFrame(1);
-		OLEDScreen.drawImage(94, 0, fan_bitmap, 16, 8, 0);
+		makeScreen(94, 0, fan_bitmap, 16, 8);
 		OLEDScreen.refresh();
 	}
 	// Run repeatly for update.
@@ -287,4 +284,34 @@ void getADLDData(void)
 	while (!tmr50ms.ready()) {}
 
 	ALDL_UART.fillBuff((uint8_t*)&ALDLData);
+}
+
+// ****************************************************************************************
+
+void makeScreen(uint8_t x, uint8_t y, const unsigned char *input, uint8_t width, uint8_t height)
+{
+	OLEDScreen.setbuf(0);
+	OLEDScreen.drawFrame(1);
+	if((int)input != 0)
+		OLEDScreen.drawImage(x, y, input, width, height, 0);
+}
+
+// ****************************************************************************************
+
+void makeStartScreen(void)
+{
+	OLEDScreen.drawImage(10, 1, vette_logo, 88, 64, 0);
+    OLEDScreen.drawchar(6, 15, 'P', 1);
+    OLEDScreen.drawchar(6, 25, 'r', 1);
+    OLEDScreen.drawchar(6, 35, 'e', 1);
+    OLEDScreen.drawchar(6, 45, 's', 1);
+    OLEDScreen.drawchar(6, 55, 's', 1);
+
+    OLEDScreen.drawchar(116, lineNumbers[0], 'B', 1);
+    OLEDScreen.drawchar(116, lineNumbers[1], 'u', 1);
+    OLEDScreen.drawchar(116, lineNumbers[2], 't', 1);
+    OLEDScreen.drawchar(116, lineNumbers[3], 't', 1);
+    OLEDScreen.drawchar(116, lineNumbers[4], 'o', 1);
+    OLEDScreen.drawchar(116, lineNumbers[5], 'n', 1);
+    OLEDScreen.drawFrame(1);
 }
